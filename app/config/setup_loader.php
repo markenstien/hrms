517b37de7e4b0f6c486b47ff3b0d13791a74e790
Mark Angelo Gonzales <markgonzalespersonal@gmail.com>
2024-01-02 17:22:11 +0800

    add setup error

--- app/config/setup_loader.php
+++ app/config/setup_loader.php
@@ -68,5 +68,9 @@
 
             define('BASECONTROLLER' , 'Login');
             define('BASEMETHOD' , 'index');
+
+            ini_set('display_errors', 1);
+            ini_set('display_startup_errors', 1);
+            error_reporting(E_ALL);
         break;
     }
